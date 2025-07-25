6527eee453b96cf4114e3b788ebe8f3599f26629
Ryan Kirk <ryankirksaith@gmail.com>
2025-07-25 02:25:07 +0000

    adicione esse código do pixel em minha página também: <script>   window.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,16 @@ export default function RootLayout({
           data-utmify-prevent-subids
           async
         />
+        <Script id="utmify-pixel" strategy="afterInteractive">
+          {`
+            window.pixelId = "6882ead2e063eac0fb1d590a";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `}
+        </Script>
       </body>
     </html>
   );
